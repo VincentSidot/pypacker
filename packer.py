acc6895d3a3077bbd8705554840b0574d36407d9
Vincent <vs38190@gmail.com>
2019-12-01 16:51:50 +0100

    added zlib compression

--- packer.py
+++ packer.py
@@ -3,30 +3,20 @@ from sys import argv
 from getpass import getpass
 import hashlib
 import base64
-
-def parcour_string(string,line_len):
-    ret = ""
-    for i,e in enumerate(string):
-        ret += e
-        if (i+1)%line_len==0:
-            yield ret
-            ret = ""
-    yield ret
-
+import zlib
 def pms_crypt(data, key):
     rep = bytearray()
     key_len = len(key)
     for i,e in enumerate(data):
         rep.append((e + i*key[i%key_len])%255)
     return bytes(rep)
-
 def encrypt_string(hash_string):
     sha_signature = hashlib.sha256(hash_string.encode()).hexdigest()
     return sha_signature
-
 base_program = """
 #!/usr/bin/python3
 import base64
+import zlib
 from getpass import getpass
 from sys import argv
 import hashlib
@@ -53,17 +43,10 @@ while encrypt_string(password)!=hashed_password:
     password = getpass('Password:')
     test2 = True
 test = False
-crypted_program = bytearray()
-for line in file.readlines():
-    if test:
-        crypted_program += base64.b64decode(line[1:-1:].encode('utf-8'))
-    if line[:-1] == '#begining':
-        test = True
-program = pms_decrypt(bytes(crypted_program),password.encode('utf-8')).decode('utf-8')
+crypted_program = base64.b64decode(b"{}")
+program = zlib.decompress(pms_decrypt(crypted_program,password.encode('utf-8'))).decode('utf-8')
 exec(program)
 """
-
-max_len = 70
 if len(argv)<3:
     print('Usage {} path_to_program path_to_packed_python password(optional)'.format(argv[0]))
     exit(-1)
@@ -78,10 +61,7 @@ else:
 
 encoded = ""
 with open(argv[1],'r') as file:
-    encoded = base64.b64encode(pms_crypt(file.read().encode('utf-8'),password.encode('utf-8'))).decode('utf-8')
+    encoded = base64.b64encode(pms_crypt(zlib.compress(file.read().encode('utf-8')),password.encode('utf-8'))).decode('utf-8')
 with open(argv[2],'w') as file:
-    file.write(base_program.format(encrypt_string(password)))
-    file.write('#begining\n')
-    for line in parcour_string(encoded,max_len):
-        file.write('#' + line + '\n')
+    file.write(base_program.format(encrypt_string(password),encoded))
 print('Everything is okay enjoy')
